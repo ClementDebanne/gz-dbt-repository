cff52f96c32b1a52c841b880353ba76bf26d39f4
Clément <clement.debanne@outlook.fr>
2025-02-11 15:02:59 +0000

    finance day

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,10 @@
+select
+    date_date
+, count(orders_id) as nb_transactions
+, round(sum(revenue),0) as revenue
+, round(avg(revenue),2) as Avereage_Basket
+, round(sum(margin),0) as margin
+, round(sum(operational_margin),0) as operational_margin
+from {{ ref("int_orders_operational")}}
+group by date_date
+order by date_date desc
